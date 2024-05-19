9c70c66574cd9044207060f5dafb42810bfd32f8
Pavlo Shchur <90649652+ShchurPavlo@users.noreply.github.com>
2024-05-19 22:13:26 +0300

    Add files via upload

---
+++ lab2/task4_1.py
@@ -0,0 +1,16 @@
+import hazelcast
+import time
+
+
+def task4_1():
+    client = hazelcast.HazelcastClient(cluster_name="ps_cluster", cluster_members=[])
+    try:
+        topic = client.get_topic("my-distributed-topic")
+        for i in range(1, 101):
+            topic.publish(i)
+            print("Sent:", i)
+            time.sleep(1)
+    finally:
+        client.shutdown()
+
+task4_1()
